6f95ff4cedb764b660b7da767d100678074f1266
Speak <1272533009@qq.com>
2019-11-19 00:10:04 +0800

    ...

--- ICourse163/C++程序设计(面向对象进阶)-崔毅东-BUPT/Unit05/DeepCopy/Date.h
+++ ICourse163/C++程序设计(面向对象进阶)-崔毅东-BUPT/Unit05/DeepCopy/Date.h
@@ -9,7 +9,6 @@ public:
 	Date() = default;
 	Date(int y, int m, int d) : year{ y }, moth{ m }, day{ d } {}
 	std::string toString() {
-		return to_string(year) + "y" + to_string(moth) + "m" + 
-			   to_string(day) + "d";
+		return to_string(year) + "y" + to_string(moth) + "m" + to_string(day) + "d";
 	}
 };
